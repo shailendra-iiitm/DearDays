9157127c99158540202fba583849cd8cfe4cb475
SHAILENDRA SHUKLA <SHAILENDRAIIITS@GMAIL.COM>
2025-06-13 11:38:51 +0530

    InitialCommit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const app=()=>{
+    return (
+        <div>
+            <h1>Welcome to the React App</h1>
+            <p>This is a simple React application.</p>
+        </div>
+    );
+}
+
+
+export default app;
