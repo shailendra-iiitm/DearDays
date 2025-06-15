913f343bff588851b2e62a65173a30a3e625f33e
SHAILENDRA SHUKLA <SHAILENDRAIIITS@GMAIL.COM>
2025-06-15 13:52:04 +0530

    Added Forms As well

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+//Adding Forn
 
 import useStyles from './styles';
 const Form = () => {
        const classes = useStyles();
     return (
-        
+        // Using the classes from styles.js
         <div>
             <h2>Form Component</h2>
             {/* Form elements will go here */}
